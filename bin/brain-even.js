f7bed767fefd9978cc31a56bf6a34fa1b36dcc66
elena <elena.sheloputova@nrg-soft.com>
2024-03-21 00:46:35 +0300

    the first game brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,35 +1,52 @@
 import readlineSync from "readline-sync";
-import { userName } from "../src/cli.js";
 
-const generateNumber = () => {
-  Math.ceil(Math.random() * 100);
+let userName;
+const askName = () => {
+  userName = readlineSync.question("May I have your name? ");
+  console.log("Hello, " + userName + "!");
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  return userName;
+};
+
+let answer;
+const roundCount = 3;
+const getRandomNumber = () => Math.round(Math.random() * 100);
+
+const isEven = (number) => !(number % 2);
+
+const checkAnswer = () => {
+  let randomNumber = getRandomNumber();
+
+  console.log("Question: " + randomNumber);
+  answer = readlineSync.question("Your answer: ");
+
+  const correctAnswer = isEven(randomNumber) ? "yes" : "no";
+
+  if (answer && answer.toLowerCase().trim() === correctAnswer) {
+    console.log("Correct!");
+    return true;
+  } else {
+    console.log(answer + " is wrong answer ;(. Correct answer was no");
+    console.log("Let's try again, " + userName + "!");
+    return false;
+  }
 };
 
 const checkEven = () => {
-  let randomNumber = generateNumber();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  console.log("Question: " + randomNumber);
-  const answer = readlineSync.question("Your answer: ");
-  console.log("Your answer: " + answer);
-
-  let i = 0;
-  while (i < 3) {
-    if (
-      (randomNumber % 2 === 0) & (answer === "yes") ||
-      (randomNumber % 2 != 0) & (answer === "no")
-    ) {
-      console.log("Correct!");
-      //console.log("Question: " + generateNumber());
-
-      i = i++;
-    } else {
-      console.log(answer + " is wrong answer ;(. Correct answer was no");
-      console.log("Let's try again, " + userName + "!");
-    }
-    if (i === 2) {
-      console.log("Congratulations, " + userName + "!");
+  for (let i = 0; i < roundCount; i++) {
+    const isCorrect = checkAnswer();
+    if (isCorrect && i === 2) {
+      console.log("Congratulations, " + userName);
+      break;
+    } else if (!isCorrect) {
+      break;
     }
   }
 };
 
-checkEven();
+const playGame = () => {
+  askName();
+  checkEven();
+};
+
+playGame();
--- makefile
+++ makefile
@@ -1,8 +1,17 @@
-brain-games:
-	node bin/brain-games.js
+install: 
+	npm ci
 
 publish:
 	npm publish --dry-run
 
 make lint:
 	npx eslint
+
+brain-games:
+	node bin/brain-games.js
+	
+brain-even:
+	node bin/brain-even.js
+
+
+
